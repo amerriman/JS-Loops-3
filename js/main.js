012ab2cb3c06e860c6a2343cdb349e03fc0ecc8e
amerriman <amm434@gmail.com>
2015-08-03 22:19:38 -0600

    uncommented solutions

--- js/main.js
+++ js/main.js
@@ -46,104 +46,104 @@ for (var i = 0; i < howOld.length; i++) {
   console.log(howOld[i]);
 }
 
-// //or
-// function ageLoop(arr) {
-//   arr.forEach(function(student) {
-//     console.log(student.age);
-//   });
-// }
-// ageLoop(students);
+//or
+function ageLoop(arr) {
+  arr.forEach(function(student) {
+    console.log(student.age);
+  });
+}
+ageLoop(students);
 
-// //or
+//or
 
-// for (var i = 0; i < students.length; i++){
-//   console.log("second solution" + students[i].age);
-// }
-
-
-// //Problem 2
-// function nameAndCity(arr) {
-//   arr.forEach(function(student) {
-//     console.log(student.name + ", " + student.city);
-//   });
-// }
-// nameAndCity(students);
-
-// //or
-
-// for (var i = 0; i < students.length; i++) {
-//   console.log(students[i].name + ", " + students[i].city);
-// }
+for (var i = 0; i < students.length; i++){
+  console.log("second solution" + students[i].age);
+}
 
 
-// //Problem 3
-// function fromBoulder(arr) {
-//   arr.forEach(function(student) {
-//     if (student.city === 'Boulder')
-//     console.log(student.name + " is from " + student.city);
-//   });
-// }
-// fromBoulder(students);
+//Problem 2
+function nameAndCity(arr) {
+  arr.forEach(function(student) {
+    console.log(student.name + ", " + student.city);
+  });
+}
+nameAndCity(students);
 
-// //or
+//or
 
-// for (var i = 0; i < students.length; i++) {
-//   if (students[i].city === 'Boulder'){
-//   console.log(students[i].name + " is from " + students[i].city);
-//   }
-// }
-
-// //or
-
-// function whereFrom(students, location){
-// var matchingStudents = [];
-//   return students.filter(function(student){
-//     if (student.city === location) {
-//       matchingStudents.push(student);
-//       return matchingStudents;
-//     }
-//   });
-// }
-
-// var boulderStudents = whereFrom(students, "Boulder");
-// for (var i = 0; i < boulderStudents.length; i++) {
-//   console.log(boulderStudents[i].name + " is from Boulder.");
-// }
-// console.log(boulderStudents);
+for (var i = 0; i < students.length; i++) {
+  console.log(students[i].name + ", " + students[i].city);
+}
 
 
+//Problem 3
+function fromBoulder(arr) {
+  arr.forEach(function(student) {
+    if (student.city === 'Boulder')
+    console.log(student.name + " is from " + student.city);
+  });
+}
+fromBoulder(students);
 
-// //Problem 4
+//or
 
-// function olderThan25(arr) {
-//   arr.forEach(function(student) {
-//     if (student.age > 25)
-//     console.log(student.name + " is older than 25.");
-//   });
-// }
-// olderThan25(students);
+for (var i = 0; i < students.length; i++) {
+  if (students[i].city === 'Boulder'){
+  console.log(students[i].name + " is from " + students[i].city);
+  }
+}
 
-// //or
+//or
 
-// for (var i = 0; i < students.length; i++) {
-//   if (students[i].age > 25){
-//   console.log(students[i].name + " is older than 25.");
-//   }
-// }
+function whereFrom(students, location){
+var matchingStudents = [];
+  return students.filter(function(student){
+    if (student.city === location) {
+      matchingStudents.push(student);
+      return matchingStudents;
+    }
+  });
+}
 
-// //or
+var boulderStudents = whereFrom(students, "Boulder");
+for (var i = 0; i < boulderStudents.length; i++) {
+  console.log(boulderStudents[i].name + " is from Boulder.");
+}
+console.log(boulderStudents);
 
-// function older25(students, ageLimit){
-// var matchingStudents = [];
-//   return students.filter(function(student){
-//     if (student.age > ageLimit) {
-//       matchingStudents.push(student);
-//       return matchingStudents;
-//     }
-//   });
-// }
 
-// var twentyFiveAndUp = older25(students, 25);
-// for (var i = 0; i < twentyFiveAndUp.length; i++) {
-//   console.log(twentyFiveAndUp[i].name + " is older than 25.");
-// }
+
+//Problem 4
+
+function olderThan25(arr) {
+  arr.forEach(function(student) {
+    if (student.age > 25)
+    console.log(student.name + " is older than 25.");
+  });
+}
+olderThan25(students);
+
+//or
+
+for (var i = 0; i < students.length; i++) {
+  if (students[i].age > 25){
+  console.log(students[i].name + " is older than 25.");
+  }
+}
+
+//or
+
+function older25(students, ageLimit){
+var matchingStudents = [];
+  return students.filter(function(student){
+    if (student.age > ageLimit) {
+      matchingStudents.push(student);
+      return matchingStudents;
+    }
+  });
+}
+
+var twentyFiveAndUp = older25(students, 25);
+for (var i = 0; i < twentyFiveAndUp.length; i++) {
+  console.log(twentyFiveAndUp[i].name + " is older than 25.");
+}
